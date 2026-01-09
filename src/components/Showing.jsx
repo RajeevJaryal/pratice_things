166cb908e2bcfc10321e0d9330df6199ed043e0d
Rajeev Kumar <rajeevkumarrj7@gmail.com>
2026-01-09 22:42:07 +0530

    added toggle button

--- src/components/Showing.jsx
+++ src/components/Showing.jsx
@@ -4,21 +4,30 @@ import { useSelector,useDispatch } from "react-redux";
 
 const Showing=()=>{
     const counter=useSelector(state=>state.counter);
+    const show=useSelector(state=>state.show);
     const dispatch=useDispatch();
 
 
+    const showCounter=()=>{
+        dispatch({type:"toggle"})
+    }
     const increment=()=>{
         dispatch({type:"increment"})
     }
-
+    const betterIncrement=()=>{
+        dispatch({type:"increment", amount:5})
+    }
     const decrement=()=>{
         dispatch({type:"decrement"})
     }
     return(
         <>
         <button onClick={decrement}>Decremenen by 5</button>
-        {counter}
+        {show && <div>{counter}</div>}
+        <button onClick={betterIncrement}> Increment better</button>
         <button onClick={increment}>Increment By 5</button>
+
+        <button onClick={showCounter}>Toggle</button>
         </>
     )
 }
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,15 +1,30 @@
 import {createStore} from "redux";
 
 
-const counterReducer=(state={counter:0},action)=>{
+const counterReducer=(state={counter:0, show:true},action)=>{
     if(action.type==="increment"){
         return{
-            counter:state.counter+5
+            counter:state.counter+5,
+            show:state.show
+        }
+    }
+    if(action.type==="increment"){
+        return{
+            counter:state.counter+action.amount,
+            show:state.show
         }
     }
     if(action.type==="decrement"){
         return{
-            counter:state.counter-5
+            counter:state.counter-5,
+            show:state.show
+        }
+    }
+    if(action.type==="toggle"){
+        return{
+            show:!state.show,
+            counter:state.counter
+
         }
     }
     return state
